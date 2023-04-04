ce50c968c409c722dee55eb0c362a033a2f54887
allegradepasquale <67483844+allegradepasquale@users.noreply.github.com>
2023-04-04 17:29:46 -0400

    Add files via upload

---
+++ Casorso et al. 2023_Phylogenetic Tree Creation.R
@@ -0,0 +1,15 @@
+#Casorso et al. 2023 - Code for Creating Phylogenetic Tree
+
+library(V.PhyloMaker)
+
+#Step 1: Import species list (read data file into a dataframe)
+df <- read.table(file = 'C:\\Users\\Casorso\\Desktop\\Casorso et al. 2023 - Species List for Phylogenetic Tree.txt', header=TRUE, blank.lines.skip = FALSE)
+print(df)
+
+#Step 2: Generate phylogeny (this creates a Newick file in your working directory folder; make sure to set your working directory to the appropriate place)
+setwd("C:/Users/jgcas/OneDrive/Fall 2020/ECOL 530 (Melin & Barclay)/Papers for Publications/Paper 1 (phylogeny, dispersal)/R Codes and Data")
+tree <- phylo.maker(sp.list = df, tree = GBOTB.extended) #takes a few minutes
+
+#Step 3: Create Newick string of tree
+write.tree(tree$scenario.3, "PhylogeneticTree.nwk") #writes into working directory
+write.tree(tree$scenario.3) #prints in R console
